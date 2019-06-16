82cbad141b18eb0d57e30030c845dd189d1727df
franziskusehmeir <38812519+franziskusehmeir@users.noreply.github.com>
2019-06-17 01:56:14 +0200

    Tutorial & Help
    Tutorial with 3 Lessons added.
    Help added.

--- js/script.js
+++ js/script.js
@@ -1,32 +1,33 @@
 document.getElementById("run").addEventListener("click", writeCodeToFile);
-document.getElementById("download").addEventListener("click", downloadFile);
+document.getElementById("tutorial").addEventListener("click", toggleTutorial);
+document.getElementById("help").addEventListener("click", toggleHelp);
 
 ownConsole = document.getElementById("consoleOutput");
 
-function writeCodeToFile(){
+function writeCodeToFile() {
     log("Aktualisieren...", "n");
 
-    // Requiring fs module in which 
-    // writeFile function is defined. 
+    // Requiring fs module in which
+    // writeFile function is defined.
     const fs = require('fs')
-      
-    // Data which will write in a file. 
+
+    // Data which will write in a file.
     let data = editor.getValue();
-      
-    // Write data in 'Output.txt' . 
-    fs.writeFile('output.txt', data, (err) => { 
-          
-        // In case of a error throw err. 
-        if (err){
+
+    // Write data in 'Output.txt' .
+    fs.writeFile('output.txt', data, (err) => {
+
+        // In case of a error throw err.
+        if (err) {
             log("Update NICHT erfolgreich!", "e");
-            throw err; 
+            throw err;
         } else {
             log("Update erfolgreich!", "s");
         }
     })
 }
 
-function log(text, type){
+function log(text, type) {
     if (type == "e") {
         ownConsole.innerHTML += "<span class='msgError'>[ERR]</span> ";
     } else if (type == "s") {
@@ -37,10 +38,59 @@ function log(text, type){
     ownConsole.innerHTML += text;
     ownConsole.innerHTML += "<br>";
 
-    
     ownConsole.scrollTop = ownConsole.scrollHeight;
 }
 
-function downloadFile(){
-    
+function toggleTutorial() {
+    var element = document.getElementById("tutoDiv");
+    var lElement = document.getElementById("blurHelp");
+    var rElement = document.getElementById("rightWrapper");
+    var elementStyle = window.getComputedStyle(element);
+
+    if (elementStyle.getPropertyValue('display') === 'none') {
+        element.style.display = 'block';
+        lElement.setAttribute('class', 'blur');
+        rElement.setAttribute('class', 'blur');
+    } else {
+        element.style.display = 'none';
+        lElement.setAttribute('class', null);
+        rElement.setAttribute('class', null);
+    }
+}
+
+function toggleHelp() {
+    var element = document.getElementById("helpDiv");
+    var lElement = document.getElementById("blurHelp");
+    var rElement = document.getElementById("rightWrapper");
+
+    if (element.style.visibility == 'visible') {
+        element.style.visibility = 'hidden';
+        lElement.setAttribute('class', null);
+        rElement.setAttribute('class', null);
+    } else {
+        element.style.visibility = 'visible';
+        lElement.setAttribute('class', 'blur');
+        rElement.setAttribute('class', 'blur');
+    }
+}
+
+var tutoNo = 1;
+
+function reset(h1) {
+    document.getElementById(h1).style.visibility = 'hidden';
+    for(let lesson of document.getElementsByClassName("lesson")){
+      lesson.style.visibility = 'hidden';
+    }
+}
+
+function increment() {
+    tutoNo++;
+    if (tutoNo >= 3) {
+        tutoNo = 3;
+    }
+
+    h1 = "l" + tutoNo;
+    reset("l" + (tutoNo - 1));
+    // var h2 = "l" + (i-1);
+    document.getElementById(h1).style.visibility = 'visible';
 }
